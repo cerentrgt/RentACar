7cb7cc2916ca24517f4fd199ac9e3ebb35fb46b3
Ceren Turgut <veyselceren25@gmail.com>
2021-03-14 19:34:02 +0300

    14.03

--- Business/Concrete/UserManager.cs
+++ Business/Concrete/UserManager.cs
@@ -38,8 +38,7 @@ namespace Business.Concrete
 
         public IDataResult<List<User>> GetAll()
         {
-            var getAll = _userDal.GetAll();
-            return new SuccessDataResult<List<User>>(getAll);
+            return new SuccessDataResult<List<User>>(_userDal.GetAll(), Messages.UserListed);
         }
 
         public IDataResult<User> GetById(int userId)
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -68,7 +68,7 @@ namespace WebAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseCors(builder=>builder.WithOrigins("http://localhost:4200").AllowAnyHeader());
+            app.UseCors(builder=>builder.WithOrigins("http://localhost:4201").AllowAnyHeader());
 
             app.UseHttpsRedirection();
 
